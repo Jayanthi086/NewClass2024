b3afaa739fefff5b0f6e5206cd84fff947510058
Jayanthi086 <jayanthi086@gmail.com>
2024-09-16 19:08:15 +0530

    new project

---
+++ src/org/test/GitHub.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class GitHub {
+	public static void main(String[] args) {
+		System.out.println("login details");
+		System.out.println("username");
+		System.out.println("password");
+		System.out.println("login account");
+	}
+
+}
